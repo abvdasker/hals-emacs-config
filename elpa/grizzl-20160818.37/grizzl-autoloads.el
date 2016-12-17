19c4dd16884e355333b37339bcd412254b7f4257
abvdasker <hal.c.rogers@gmail.com>
2016-12-16 23:14:37 -0500

    Removes stuff

--- elpa/grizzl-20160818.37/grizzl-autoloads.el
+++ elpa/grizzl-20160818.37/grizzl-autoloads.el
@@ -1,9 +1,9 @@
 ;;; grizzl-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "grizzl" "grizzl.el" (22542 7960 0 0))
+;;;### (autoloads nil "grizzl" "grizzl.el" (22612 48000 0 0))
 ;;; Generated autoloads from grizzl.el
 
 (autoload 'grizzl-make-index "grizzl" "\
--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 (ido-mode t)
 
 ;; load theme
-;;(load-theme 'darkokai t)
+(load-theme 'darkokai t)
 
 ;; bind auto-indent to right brace
 (global-set-key (kbd "C-M-]") 'indent-region)
@@ -70,3 +70,17 @@
 
 (require 'tramp)
 (setq tramp-default-method "ssh")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (grizzl lispyscript-mode thrift projectile markdown-mode json-mode go-mode exec-path-from-shell elixir-mode darkokai-theme csv-mode ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
