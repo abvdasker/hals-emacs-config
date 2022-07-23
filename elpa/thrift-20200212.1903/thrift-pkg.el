ef51563a7e50d32e966c961124cc42f1ec719265
Hal Rogers <halrogers@Hals-MacBook-Air.local>
2022-07-23 00:58:52 -0400

    upgrade packages

---
+++ elpa/thrift-20200212.1903/thrift-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from thrift.el  -*- no-byte-compile: t -*-
+(define-package "thrift" "20200212.1903" "major mode for fbthrift and Apache Thrift files" '((emacs "24")) :commit "09c78989d06249a6921edef812130006c4a11029" :keywords '("languages"))
